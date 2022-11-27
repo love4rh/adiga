e4341398069785555d5ba1831085d3b3d7ec1e77
Myunghyun Kim <love4rh@paran.com>
2022-11-27 11:36:54 +0900

    initial commit

---
+++ runAll.sh
@@ -0,0 +1,5 @@
+java -Dfile.encoding=utf8 -Duser.timezone=GMT -jar ./crawlego-1.0.0-m8.jar "./01.adiga-get-univ.xml" "A=A"
+java -Dfile.encoding=utf8 -Duser.timezone=GMT -jar ./crawlego-1.0.0-m8.jar "./02.adiga-get-division.xml" "A=A"
+java -Dfile.encoding=utf8 -Duser.timezone=GMT -jar ./crawlego-1.0.0-m8.jar "./03-1.adiga-get-result01.xml" "A=A"
+java -Dfile.encoding=utf8 -Duser.timezone=GMT -jar ./crawlego-1.0.0-m8.jar "./03-2.adiga-get-result02.xml" "A=A"
+java -Dfile.encoding=utf8 -Duser.timezone=GMT -jar ./crawlego-1.0.0-m8.jar "./04.adiga-mgr-data.xml" "A=A"
